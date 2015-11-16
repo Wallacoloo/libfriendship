7b08ccb512d3875c4d6a43c594be9fd83e3c4769
Colin Wallace <wallacoloo@gmail.com>
2015-11-16 00:03:32 -0800

    Simplify ordering for Nodes

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -12,6 +12,7 @@ type NodeId = u32;
 ///   A1 (a sum of automations)
 ///   A2 (a sum of automations)
 /// Provides one output: A1*A2, which is a sum of automations
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct ANode {
     id: NodeId,
 }
@@ -20,10 +21,12 @@ pub struct ANode {
 ///   Y (a sum of partials)
 ///   A (a sum of automations)
 /// Provides one output: Y*A, which is a sum of partials
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct YNode {
     id: NodeId,
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub enum Node {
     ANode(ANode),
     YNode(YNode),
@@ -49,20 +52,11 @@ impl Node {
         };
         id as u32
     }
-    fn id(&self) -> NodeId {
-        match self {
-            &Node::ANode(ref anode) => anode.id(),
-            &Node::YNode(ref ynode) => ynode.id(),
-        }
-    }
 }
 
 impl ANode {
     pub fn new() -> ANode {
         ANode{ id: Node::consume_next_id() }
-    }
-    fn id(&self) -> NodeId {
-        self.id
     }
 }
 
@@ -70,70 +64,4 @@ impl YNode {
     pub fn new() -> YNode {
         YNode{ id: Node::consume_next_id() }
     }
-    fn id(&self) -> NodeId {
-        self.id
-    }
 }
-
-
-// Implement comparisons between Nodes so that they can be orderable.
-impl Ord for Node {
-    fn cmp(&self, other: &Node) -> Ordering {
-        self.id().cmp(&other.id())
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Node {
-    fn eq(&self, other: &Node) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-impl Eq for Node {}
-
-
-// Comparisons for ANode
-impl Ord for ANode {
-    fn cmp(&self, other: &ANode) -> Ordering {
-        self.id().cmp(&other.id())
-    }
-}
-
-impl PartialOrd for ANode {
-    fn partial_cmp(&self, other: &ANode) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for ANode {
-    fn eq(&self, other: &ANode) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-impl Eq for ANode {}
-
-
-// Comparisons for YNode
-impl Ord for YNode {
-    fn cmp(&self, other: &YNode) -> Ordering {
-        self.id().cmp(&other.id())
-    }
-}
-
-impl PartialOrd for YNode {
-    fn partial_cmp(&self, other: &YNode) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for YNode {
-    fn eq(&self, other: &YNode) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-impl Eq for YNode {}
