5deedf08743c7032a8fdcdb334470acdab396141
Colin Wallace <wallacoloo@gmail.com>
2015-11-15 21:56:10 -0800

    Expect send/node implementation (add c'tors & accessors, plus make Nodes RC'd)

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,22 +1,37 @@
-/// Takes two inputs:
-///   Y (a sum of partials)
-///   A (a sum of automations)
-/// Provides one output: Y*A, which is a sum of partials
-pub struct YNode;
+
 /// Takes two inputs:
 ///   A1 (a sum of automations)
 ///   A2 (a sum of automations)
 /// Provides one output: A1*A2, which is a sum of automations
 pub struct ANode;
 
+/// Takes two inputs:
+///   Y (a sum of partials)
+///   A (a sum of automations)
+/// Provides one output: Y*A, which is a sum of partials
+pub struct YNode;
+
 pub enum Node {
-	ANode(ANode),
-	YNode(YNode),
+    ANode(ANode),
+    YNode(YNode),
 }
 
 /// Used primarily in Sends; Automations can either be sent to the *left* input
 /// of an ANode, or to the right input.
+#[derive(Clone, Copy, Debug)]
 pub enum NodeInputSlot {
-	Left,
-	Right,
+    Left,
+    Right,
+}
+
+impl ANode {
+    pub fn new() -> ANode {
+        ANode
+    }
+}
+
+impl YNode {
+    pub fn new() -> YNode {
+        YNode
+    }
 }
--- src/tree/send.rs
+++ src/tree/send.rs
@@ -1,29 +1,72 @@
+use std::rc::Rc;
+
 use super::node::{ANode, NodeInputSlot, YNode};
 
-/// Sends a partial stream from the output of a YNode to the (left) input of
-///   another YNode.
-pub struct YYSend {
-	src: YNode,
-	dest: YNode,
+/// Sends an automation stream from the output of an ANode to the input of
+/// another ANode (either the left or right slot, as specified)
+pub struct AASend {
+    src: Rc<ANode>,
+    dest: Rc<ANode>,
+    dest_slot: NodeInputSlot,
 }
 
 /// Sends an automation stream from the output of a ANode to the (right) input
 /// of a YNode
 pub struct AYSend {
-	src: ANode,
-	dest: YNode,
+    src: Rc<ANode>,
+    dest: Rc<YNode>,
 }
 
-/// Sends an automation stream from the output of an ANode to the input of
-/// another ANode (either the left or right slot, as specified)
-pub struct AASend {
-	src: ANode,
-	dest: ANode,
-	dest_slot: NodeInputSlot,
+/// Sends a partial stream from the output of a YNode to the (left) input of
+///   another YNode.
+pub struct YYSend {
+    src: Rc<YNode>,
+    dest: Rc<YNode>,
 }
 
 pub enum Send {
-	AASend(AASend),
-	AYSend(AYSend),
-	YYSend(YYSend),
+    AASend(AASend),
+    AYSend(AYSend),
+    YYSend(YYSend),
 }
+
+impl AASend {
+    pub fn new(src: Rc<ANode>, dest: Rc<ANode>, dest_slot: NodeInputSlot)
+    -> AASend {
+        AASend{ src: src, dest:dest, dest_slot: dest_slot }
+    }
+    pub fn src(&self) -> &Rc<ANode> {
+        &self.src
+    }
+    pub fn dest(&self) -> &Rc<ANode> {
+        &self.dest
+    }
+    pub fn dest_slot(&self) -> NodeInputSlot {
+        self.dest_slot
+    }
+}
+
+impl AYSend {
+    pub fn new(src: Rc<ANode>, dest: Rc<YNode>) -> AYSend {
+        AYSend{ src: src, dest:dest }
+    }
+    pub fn src(&self) -> &Rc<ANode> {
+        &self.src
+    }
+    pub fn dest(&self) -> &Rc<YNode> {
+        &self.dest
+    }
+}
+
+impl YYSend {
+    pub fn new(src: Rc<YNode>, dest: Rc<YNode>) -> YYSend {
+        YYSend{ src: src, dest:dest }
+    }
+    pub fn src(&self) -> &Rc<YNode> {
+        &self.src
+    }
+    pub fn dest(&self) -> &Rc<YNode> {
+        &self.dest
+    }
+}
+
