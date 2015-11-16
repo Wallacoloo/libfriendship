24dcd035e92ee67389625bcb697459545a4d16fb
Colin Wallace <wallacoloo@gmail.com>
2015-11-16 02:34:15 -0800

    next() function takes no parameters. If the caller gets an unexpected node, they may ignore it.

--- src/tree/tree.rs
+++ src/tree/tree.rs
@@ -14,7 +14,7 @@ trait Tree {
     fn feed_a(&mut self, a: Automation, src: Rc<ASrcNode>);
     /// add Partial external stimuli to the tree.
     fn feed_y(&mut self, y: Partial, src: Rc<YSrcNode>);
-    /// Poll the next Partial to arrive at the provided Sink.
-    /// Will return None if there are no new partials to be read.
-    fn sink_y(&mut self, sink: Rc<YSinkNode>) -> Option<Partial>;
+    /// Poll the next Partial to arrive at any sink.
+    /// Will return None if there are no new partials to be output.
+    fn next(&mut self) -> Option<Rc<YSinkNode>, Partial>;
 }
