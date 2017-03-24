f25b120e6bdf9c83bd054a71f24a4ed780927982
Colin Wallace <wallacoloo@gmail.com>
2017-03-24 01:22:56 -0700

    Impl node/edge addition and removal

--- src/routing/routegraph.rs
+++ src/routing/routegraph.rs
@@ -9,6 +9,10 @@ use self::online_dag::iodag::IODag;
 
 use super::effect::Effect;
 
+type DagImpl=IODag<NodeData, EdgeWeight>;
+pub type NodeHandle=iodag::NodeHandle;
+pub type Edge=iodag::Edge<EdgeWeight>;
+
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
 struct EdgeWeight {
     from_slot: u32,
@@ -22,9 +26,6 @@ struct NodeData {
 }
 
 
-type DagImpl=IODag<NodeData, EdgeWeight>;
-pub type NodeHandle=iodag::NodeHandle;
-pub type Edge=iodag::Edge<EdgeWeight>;
 pub struct RouteGraph {
     dag: DagImpl,
 }
@@ -35,41 +36,13 @@ impl RouteGraph {
             dag: DagImpl::new(),
         }
     }
-    /*pub fn iter_topo_rev(&self) -> impl Iterator<Item=poscostdag::NodeHandle<RouteNode, RouteEdge>> {
-        self.dag.iter_topo_rev(&self.root)
-    }*/
-    /*pub fn children_of(&self, of: &RouteNodeHandle) -> impl Iterator<Item=RouteHalfEdge> {
-        self.dag.children(of)
+    pub fn add_node(&mut self, node: NodeData) -> NodeHandle {
+        self.dag.add_node(node)
     }
-    pub fn add_node(&mut self, data: RouteNode) -> RouteNodeHandle {
-        self.dag.add_node(data)
+    pub fn add_edge(&mut self, edge: Edge) -> Result<(), ()> {
+        self.dag.add_edge(edge)
     }
-    pub fn add_edge(&mut self, from: &RouteNodeHandle, to: &RouteNodeHandle, data: RouteEdge) -> Result<(), ()> {
-        self.dag.add_edge(from, to, data)
+    pub fn del_edge(&mut self, edge: Edge) {
+        self.dag.del_edge(edge)
     }
-    pub fn rm_edge(&mut self, from: &RouteNodeHandle, to: &RouteNodeHandle, data: RouteEdge) {
-        self.dag.rm_edge(from, to, data);
-    }
-    /// Return only the inputs into the left (i.e. non-delayed) channel of `of`
-    pub fn left_children_of(&self, of: &RouteNodeHandle) -> impl
-      Iterator<Item=RouteHalfEdge> {
-        self.dag.children(of).filter(|edge| edge.weight().is_left())
-    }
-    /// Return only the inputs into the right (i.e. non-delayed) channel of `of`
-    pub fn right_children_of(&self, of: &RouteNodeHandle) -> impl
-      Iterator<Item=RouteHalfEdge> {
-        self.dag.children(of).filter(|edge| edge.weight().is_right())
-    }
-    /// Returns 1 + the index of the highest channel number.
-    /// e.g. if we have ch0 and ch2 (no ch1), this returns 3.
-    pub fn n_channels(&self) -> u32 {
-        self.right_children_of(&self.root())
-            .map(|edge| edge.weight().slot_idx())
-            .max()
-            .unwrap_or(0u32) // default no channels
-    }
-    pub fn make_channel_output(&mut self, node: &RouteNodeHandle, ch: u32) {
-        let root = &self.root.clone();
-        self.add_edge(&root, &node, RouteEdge::new_right(ch));
-    }*/
 }
