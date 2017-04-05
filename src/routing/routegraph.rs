da379a8cfef66784a9ee5285e659c3c8b4e016c7
Colin Wallace <wallacoloo@gmail.com>
2017-04-05 15:39:56 -0700

    Begin flattening the RouteGraph implementation;
    Moved all functionality from the online_dag crate into routegraph.rs;
    still need to implement add_edge & a handful of relatives

--- src/routing/routegraph.rs
+++ src/routing/routegraph.rs
@@ -3,20 +3,14 @@
 /// Edges are also allowed to go to null, in which case they only have a destination slot and
 /// channel. These are outputs.
 /// Edges can also COME from null, in which case the source has the format (slot, channel)
-extern crate online_dag;
-use self::online_dag::iodag;
-use self::online_dag::iodag::IODag;
 
 use super::effect::Effect;
 use super::graphwatcher::GraphWatcher;
 
 use std::collections::hash_set::HashSet;
 use std::collections::hash_map::HashMap;
+use std::collections::hash_map;
 use std::rc::Rc;
-
-type DagImpl=IODag<NodeData, EdgeWeight>;
-type PrimNodeHandle=iodag::NodeHandle;
-type PrimEdge=iodag::Edge<EdgeWeight>;
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
 struct EdgeWeight {
@@ -37,61 +31,91 @@ pub struct DagHandle {
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
+pub struct PrimNodeHandle {
+    id: u64,
+}
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
 pub struct NodeHandle {
     dag_handle: DagHandle,
-    node_handle: PrimNodeHandle,
+    node_handle: Option<PrimNodeHandle>,
 }
 
 #[derive(Clone, Hash, Eq, PartialEq)]
 pub struct Edge {
     dag_handle: DagHandle,
-    edge: PrimEdge,
+    from: Option<PrimNodeHandle>,
+    to: Option<PrimNodeHandle>,
+    weight: EdgeWeight,
 }
 
 
 pub struct RouteGraph {
+    // TODO: can make these non-zero for more efficient Option<NodeHandle> encoding
     dag_counter: u32,
-    dags: HashMap<DagHandle, DagImpl>,
+    node_counter: u64,
     watchers: Vec<Box<GraphWatcher>>,
+    edges: HashMap<NodeHandle, EdgeSet>,
+    node_data: HashMap<NodeHandle, NodeData>,
+}
+
+struct EdgeSet {
+    outbound: HashSet<Edge>,
+    inbound: HashSet<Edge>,
 }
 
 impl RouteGraph {
     pub fn new() -> Self {
         RouteGraph {
             dag_counter: 0,
-            dags: HashMap::new(),
+            node_counter: 0,
             watchers: Vec::new(),
+            edges: HashMap::new(),
+            node_data: HashMap::new(),
         }
     }
     pub fn add_watcher(&mut self, mut watcher: Box<GraphWatcher>, do_replay: bool) {
         if do_replay {
-            for (dag_handle, dag) in self.dags.iter() {
-                for node in dag.iter_nodes() {
-                    watcher.on_add_node(&NodeHandle::new(*dag_handle, *node));
-                }
-                for edge in dag.iter_edges() {
-                    watcher.on_add_edge(&Edge::new(*dag_handle, edge.clone()));
-                }
+            for node in self.iter_nodes() {
+                watcher.on_add_node(node);
+            }
+            for edge in self.iter_edges() {
+                watcher.on_add_edge(edge);
             }
         }
         self.watchers.push(watcher);
     }
+    pub fn iter_nodes<'a>(&'a self) -> impl Iterator<Item=&NodeHandle> + 'a {
+        self.node_data.keys()
+    }
+    pub fn iter_edges<'a>(&'a self) -> impl Iterator<Item=&Edge> + 'a {
+        self.edges.values().flat_map(|v_set| v_set.outbound.iter())
+    }
     pub fn add_dag(&mut self) -> DagHandle {
-        let handle = DagHandle{ id: self.dag_counter};
+        let handle = DagHandle{ id: self.dag_counter };
         self.dag_counter += 1;
-        self.dags.insert(handle, DagImpl::new());
         handle
     }
-    pub fn add_node(&mut self, dag: DagHandle, node: NodeData) -> NodeHandle {
-        let inner_handle = self.dags.get_mut(&dag).unwrap().add_node(node);
-        let handle = NodeHandle::new(dag, inner_handle);
+    pub fn add_node(&mut self, dag: DagHandle, node_data: NodeData) -> NodeHandle {
+        let primhandle = PrimNodeHandle { id: self.node_counter };
+        let handle = NodeHandle {
+            dag_handle: dag,
+            node_handle: Some(primhandle),
+        };
+        self.node_counter = self.node_counter+1;
+        // Create storage for the node's outgoing edges
+        // Panic if the NodeHandle was somehow already in use.
+        assert!(self.edges.insert(handle, EdgeSet::new()).is_none());
+        // Store the node's data
+        assert!(self.node_data.insert(handle, node_data).is_none());
         for w in &mut self.watchers {
             w.on_add_node(&handle);
         }
         handle
     }
     pub fn add_edge(&mut self, edge: Edge) -> Result<(), ()> {
-        let ok_to_add = {
+        unimplemented!();
+        /*let ok_to_add = {
             let ref dag = self.dags[edge.dag_handle()];
             dag.can_add_edge(edge.edge(),
                 &|e1, e2| self.are_node_slots_connected(
@@ -109,12 +133,13 @@ impl RouteGraph {
             let mut mdag = self.dags.get_mut(edge.dag_handle()).unwrap();
             mdag.add_edge_unchecked(edge.edge);
         }
-        ok_to_add
+        ok_to_add*/
     }
-    fn toplevel_dag(&self) -> &DagImpl {
+    /*fn toplevel_dag(&self) -> &DagImpl {
         &self.dags[&DagHandle{ id: 0 }]
-    }
-    fn are_node_slots_connected(&self, data: &NodeData, in_slot: u32, in_ch: u8, out_slot: u32, out_ch: u8) -> bool {
+    }*/
+    /*fn are_node_slots_connected(&self, data: &NodeData, in_slot: u32, in_ch: u8, out_slot: u32, out_ch: u8) -> bool {
+        unimplemented!();
         match *data {
             // TODO: for now, consider all inputs tied to all outputs for each graph.
             // In future, may enforce constraints or actually calculate the connections,
@@ -122,9 +147,11 @@ impl RouteGraph {
             NodeData::Graph(ref dag_handle) => true,
             NodeData::Effect(ref effect) => effect.are_slots_connected(in_slot, in_ch, out_slot, out_ch)
         }
-    }
+    }*/
     /// Returns true if there's a path from `in` to `out`.
     pub fn are_slots_connected(&self, in_slot: u32, in_ch: u8, out_slot: u32, out_ch: u8) -> bool {
+        unimplemented!();
+        /*
         let mut are_connected = false;
         assert!(self.dags.len() == 1); // no nested DAGs
         self.toplevel_dag().traverse(&mut |edge| {
@@ -136,20 +163,39 @@ impl RouteGraph {
             }
             do_follow
         });
-        are_connected
+        are_connected*/
     }
     pub fn del_node(&mut self, node: NodeHandle) -> Result<(), ()> {
-        let result = self.dags.get_mut(node.dag_handle()).unwrap().del_node(*node.node_handle());
-        // only notify watchers on a successful operation.
-        if let Ok(_) = result {
+        let ok_to_delete = match self.edges.entry(node) {
+            // Already deleted
+            hash_map::Entry::Vacant(_) => Ok(()),
+            hash_map::Entry::Occupied(entry) => {
+                if entry.get().is_empty() {
+                    entry.remove();
+                    Ok(())
+                } else {
+                    // Node has edges
+                    Err(())
+                }
+            }
+        };
+        if let Ok(_) = ok_to_delete {
+            // delete the data associated with this node
+            self.node_data.remove(&node);
+            // notify watchers of successful deletion
             for w in &mut self.watchers {
                 w.on_del_node(&node);
             }
         }
-        result
+        ok_to_delete
     }
     pub fn del_edge(&mut self, edge: Edge) {
-        self.dags.get_mut(edge.dag_handle()).unwrap().del_edge(edge.edge().clone());
+        if let Some(edge_set) = self.edges.get_mut(&edge.from_full()) {
+            edge_set.outbound.remove(&edge);
+        }
+        if let Some(edge_set) = self.edges.get_mut(&edge.to_full()) {
+            edge_set.inbound.remove(&edge);
+        }
         for w in &mut self.watchers {
             w.on_del_edge(&edge);
         }
@@ -157,7 +203,7 @@ impl RouteGraph {
 }
 
 impl NodeHandle {
-    fn new(dag: DagHandle, node: PrimNodeHandle) -> Self {
+    fn new(dag: DagHandle, node: Option<PrimNodeHandle>) -> Self {
         Self {
             dag_handle: dag,
             node_handle: node,
@@ -166,23 +212,33 @@ impl NodeHandle {
     fn dag_handle(&self) -> &DagHandle {
         &self.dag_handle
     }
-    fn node_handle(&self) -> &PrimNodeHandle {
+    fn node_handle(&self) -> &Option<PrimNodeHandle> {
         &self.node_handle
     }
 }
 
 impl Edge {
-    fn new(dag: DagHandle, edge: PrimEdge) -> Self {
-        Self {
+    fn new(dag: DagHandle) -> Self {
+        unimplemented!();
+        /*Self {
             dag_handle: dag,
             edge: edge,
-        }
+        }*/
     }
     fn dag_handle(&self) -> &DagHandle {
         &self.dag_handle
     }
-    fn edge(&self) -> &PrimEdge {
-        &self.edge
+    fn from_full(&self) -> NodeHandle {
+        NodeHandle {
+            dag_handle: self.dag_handle,
+            node_handle: self.from,
+        }
+    }
+    fn to_full(&self) -> NodeHandle {
+        NodeHandle {
+            dag_handle: self.dag_handle,
+            node_handle: self.to,
+        }
     }
 }
 
@@ -192,5 +248,17 @@ impl NodeData {
             NodeData::Effect(_) => true,
             _ => false,
         }
+    }
+}
+
+impl EdgeSet {
+    fn new() -> Self {
+        EdgeSet {
+            outbound: HashSet::new(),
+            inbound: HashSet::new(),
+        }
+    }
+    fn is_empty(&self) -> bool {
+        self.outbound.is_empty()
     }
 }
