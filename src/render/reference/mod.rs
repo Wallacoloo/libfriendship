b9a09d6a325b1acfd435bdbac1285d5d445fe52e
Colin Wallace <wallacoloo@gmail.com>
2017-05-03 19:43:17 -0700

    Move reference renderer out of a module.

--- src/render/reference/mod.rs
+++
@@ -1,6 +0,0 @@
-/// This serves as the "reference" audio renderer.
-/// i.e. it aims to be simple and easy to understand, with little care
-/// towards resource usage.
-
-mod renderer;
-pub use self::renderer::RefRenderer;
