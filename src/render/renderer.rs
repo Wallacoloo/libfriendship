d54f67e49f50a0350bcf66ac8b1bddaf87886d18
Colin Wallace <wallacoloo@gmail.com>
2017-05-19 20:00:47 -0700

    Remove outdated TODO

--- src/render/renderer.rs
+++ src/render/renderer.rs
@@ -2,7 +2,6 @@ use routing::GraphWatcher;
 /// Trait that allows for rendering a `RouteGraph`
 pub trait Renderer: GraphWatcher {
     fn get_sample(&mut self, idx: u64, slot: u32) -> f32;
-    // TODO: use a multidimensional array type for buffer; remove num_slots param
     /// Fill the provided buffer with samples from a specific slot.
     fn fill_buffer(&mut self, buff: &mut [f32], idx: u64, slot: u32) {
         for (buff_idx, sample) in buff.iter_mut().enumerate() {
